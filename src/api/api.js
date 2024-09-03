380992f8dc0c9b45865883f57a7dbd9fae9e629e
azrappp <118091170+azrappp@users.noreply.github.com>
2024-09-03 22:16:55 +0700

    fetch api and implement in each card

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,36 @@
-export async function fetchPopularMovies() {
+export async function fetchMovies({
+  type = "popular",
+  page = 1,
+  language = "en-US",
+} = {}) {
+  const validTypes = ["popular", "top_rated"];
+
+  if (!validTypes.includes(type)) {
+    throw new Error(
+      `Invalid type provided. Expected one of: ${validTypes.join(", ")}`
+    );
+  }
+
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYzcxNTRmZDVjZDdhNDQxMmQwMDE4NDRhY2FkYzc4ZCIsIm5iZiI6MTcyNDg4NTU5Ny4wNTI4MDIsInN1YiI6IjY2Y2RlMWE5ZDhmNGZhMTBhMzYzYTM5NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.oGVmNjYidpyNxW9qEGwxDUhB3mw24BPNVFX-BkiFHrM",
+    },
+  };
+
+  const endpoint = `https://api.themoviedb.org/3/movie/${type}?language=${language}&page=${page}`;
+
+  const response = await fetch(endpoint, options);
+  if (!response.ok) {
+    throw new Error(`Failed to fetch ${type} movies`);
+  }
+
+  return await response.json();
+}
+
+export async function fetchGenres() {
   const options = {
     method: "GET",
     headers: {
@@ -9,11 +41,11 @@ export async function fetchPopularMovies() {
   };
 
   const response = await fetch(
-    "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1",
+    "https://api.themoviedb.org/3/genre/movie/list?language=en-US",
     options
   );
   if (!response.ok) {
-    throw new Error("Failed to fetch movies");
+    throw new Error("Failed to fetch genres");
   }
 
   return await response.json();
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,173 @@
-
 <script>
- import { onMount } from 'svelte';
- import { fetchPopularMovies } from '../api/api.js';
-//  import { MovieCard } from '../components/MovieCard.svelte';
- let movies = [];
- let error = null;
+  import { onMount } from 'svelte';
+  import { fetchMovies , fetchGenres} from '../api/api.js';
 
- onMount(async () => {
+  let popularMovies = [];
+  let homeMovies = [];
+  let topRatedMovies = [];
+  let genres = [];
+  let errorPopular = null;
+  let errorTopRated = null;
+  let errorGenres = null;
+  let activeMovieIndex = 0;
+
+  onMount(async () => {
     try {
-      const data = await fetchPopularMovies();
-      console.log(data.results)
-      movies = data.results;
+      const dataGenres = await fetchGenres(); // Ambil daftar genre
+      genres = dataGenres.genres;
+      const dataPopular = await fetchMovies({type:'popular'}); // Ambil film populer
+      popularMovies = dataPopular.results; 
+      homeMovies =  dataPopular.results.slice(0,4);
+      console.log(homeMovies)
+      const dataTopRated = await fetchMovies({type:'top_rated'}); // Ambil film dengan rating tertinggi
+      topRatedMovies = dataTopRated.results; // Ambil semua film top-rated
     } catch (err) {
-      error = err.message;
+      if (popularMovies.length === 0) {
+        errorPopular = err.message;
+      }
+      if (topRatedMovies.length === 0) {
+        errorTopRated = err.message;
+      }
+      console.error(err);
+      if (genres.length === 0) {
+        errorGenres = err.message;
+      }
       console.error(err);
     }
   });
-//   console.log(movies)
+
+  const setActiveMovie = (index) => {
+    activeMovieIndex = index;
+  };
+
+  const getFontSize = (title) => {
+  const words = title.split(' ');
+  const maxWordLength = Math.max(...words.map(word => word.length));
+  if (maxWordLength > 9) {
+    return 'text-2xl';
+  } else {
+    return 'text-4xl';
+  }
+};
+
+  const getGenreNames = (genreIds) => {
+    return genreIds.map(id => {
+      const genre = genres.find(g => g.id === id);
+      return genre ? genre.name : 'Unknown';
+    }).join(' / ');
+  };
 </script>
 
+<style>
+  .scrollbar-hidden::-webkit-scrollbar {
+  display: none;
+}
+
+</style>
 <nav class="font-sans flex flex-row border-b border-black border-opacity-10 bg-white backdrop-blur-lg bg-opacity-10 w-full px-8 py-4 items-center justify-between">
-    <div class="text-black font-bold">WatchMovie</div>
-    <div class="flex flex-row gap-x-4">
-        <a href="" class="text-black font-normal font-san  text-sm">Movie</a>
-        <a href="" class="text-black font-normal font-sans text-sm">Tv Series</a>
-        <a href="" class="text-black font-normal font-sans text-sm">Anime</a>
-        <a href="" class="text-black font-normal font-sans text-sm">Friends</a>
-    </div>
-    <div class=""></div>
+  <div class="text-black font-bold">WatchMovie</div>
+  <div class="flex flex-row gap-x-4">
+    <a href="/movie" class="text-black font-normal font-san text-sm">Movie</a>
+    <a href="/tvseries" class="text-black font-normal font-sans text-sm">Tv Series</a>
+    <a href="/anime" class="text-black font-normal font-sans text-sm">Anime</a>
+    <a href="/friend" class="text-black font-normal font-sans text-sm">Friends</a>
+  </div>
 </nav>
 
-<main>
-    <div>
-        <h2>Movies Today</h2>
-        <div class="flex flex-row gap-x-2 overflow-hidden">
-            {#if error}
-  <p class="text-red-500">Error: {error}</p>
-        {:else}
-            {#each movies.slice(0, 6) as movie}
-            <div class="flex flex-col mx-autorounded overflow-hidden shadow-lg ">
-              <div class="relative w-12 h-16">
-                <img class="w-full h-full object-cover" src={"https://image.tmdb.org/t/p/w500" + movie.poster_path} alt={movie.title}>
-              </div>
-                <div class="px-6 py-4">
-                  <div class="font-bold text-xl mb-2 text-white">{movie.title}</div>
-                  <p class="text-white text-base">
-                    {movie.overview.length > 100 ? movie.overview.slice(0, 100) + '...' : movie.overview}
-                  </p>
-                </div>
-                <div class="px-6 pt-4 pb-2">
-                  <span class="text-white">Rating: {movie.vote_average}</span>
-                </div>
-              </div>
-            {/each}
-       {/if}
-        </div>
+<main class="mx-8 md:mx-16 lg:mx-[80px] pt-6 items-center">
+  <!-- Hero Section -->
+  <div class="relative h-[545px] w-full flex items-center text-left bg-cover bg-center rounded-2xl"
+    style="background-image: url('https://image.tmdb.org/t/p/w1280{homeMovies[activeMovieIndex]?.backdrop_path}');">
+    <div class="font-sans absolute top-0 right-0 bottom-0 left-0 bg-gradient-to-r from-gray-900 to-gray-700/10 rounded-lg"></div>
+    <div class="flex flex-col justify-between w-full h-full py-16 mx-11">
+      <div class="flex justify-end">
+        {#if !errorPopular && homeMovies.length > 0}
+          <div class=" md:px-7 z-10 flex flex-col items-start gap-2 max-w-[402px]">
+            <p class="line-clamp-2 text-ellipsis overflow-hidden font-normal text-white text-xl">
+              {getGenreNames(popularMovies[activeMovieIndex].genre_ids)}
+            </p>
+            <p class="line-clamp-2 text-ellipsis overflow-hidden font-bold text-white text-4xl">
+              {homeMovies[activeMovieIndex].title}
+            </p>
+            <p class="text-xl text-white">⭐{homeMovies[activeMovieIndex].vote_average}</p>
+            <p class="font-grey text-gray-300 font-normal text-xl line-clamp-4">
+              {homeMovies[activeMovieIndex].overview}
+            </p>
+            <button class="px-3 py-2 text-black font-semibold bg-yellow-500 text-xl rounded-md">Add to Playlist +</button>
+          </div>
+        {:else if errorPopular}
+          <p class="text-red-500">Error: {errorPopular}</p>
+        {/if}
+      </div>
+      <div class="flex flex-row gap-x-1 justify-center">
+        {#each homeMovies as movie, index}
+        <button
+          class="w-10 h-2 bg-white opacity-60 cursor-pointer {index === activeMovieIndex ? 'bg-opacity-100' : 'bg-opacity-20 '}"
+          on:click={() => setActiveMovie(index)}
+        ></button>
+      {/each}
+      </div>
     </div>
+  </div>
+
+
+    <!-- Movies Section -->
+    <div>
+      <h2 class="font-bold text-black hide-scrollbar text-4xl my-5">Film Popular</h2>
+      <div class="container flex flex-row scrollbar-hidden hide-scrollbar overflow-x-auto gap-x-2">
+        {#if errorPopular}
+          <p class="text-red-500">Error: {errorPopular}</p>
+        {:else}
+          {#each popularMovies as movie}
+            <div class="relative h-[367px] w-60 flex-shrink-0 flex items-end justify-start text-left bg-cover bg-center rounded-2xl"
+              style="background-image: url('https://image.tmdb.org/t/p/w500{movie.poster_path}');">
+              <div class="absolute top-0 right-0 bottom-0 left-0 bg-gradient-to-b from-transparent to-gray-900 rounded-lg"></div>
+  
+              <div class="flex mx-4 w-full flex-col mb-5 z-10 gap-y-1">
+                <div class="flex flex-row items-end gap-x-1 flex-shrink">
+                  <p class={`font-bold text-white ${getFontSize(movie.title)}`}>{movie.title}</p>
+                  <p class="font-light text-white text-sm">{movie.release_date.slice(0, 4)}</p>
+                </div>
+                <div class="flex flex-row justify-between items-end">
+                  <div class="flex flex-col gap-y-0.5">
+                    <p class="text-xs font-light text-white">⭐{movie.vote_average}</p>
+                    <p class="ms-1 text-xs font-light text-white">{movie.vote_count} voted</p>
+                  </div>
+                  <button class="px-2 h-6 bg-yellow-500 font-medium text-black">+</button>
+                </div>
+              </div>
+            </div>
+          {/each}
+        {/if}
+      </div>        
+    </div>
+  <!-- Movies Section -->
+  <div>
+    <h2 class="font-bold text-black text-4xl my-5">Film Terbaik</h2>
+    <div class="container flex flex-row scrollbar-hidden overflow-x-auto gap-x-2">
+      {#if errorTopRated}
+        <p class="text-red-500">Error: {errorTopRated}</p>
+      {:else if topRatedMovies.length > 0}
+        {#each topRatedMovies as movie}
+          <div class="relative h-[367px] w-60 flex-shrink-0 flex items-end justify-start text-left bg-cover bg-center rounded-2xl"
+            style="background-image: url('https://image.tmdb.org/t/p/w500{movie.poster_path}');">
+            <div class="absolute top-0 right-0 bottom-0 left-0 bg-gradient-to-b from-transparent to-gray-900 rounded-lg"></div>
+            <div class="flex mx-4 w-full flex-col mb-5 z-10 gap-y-1">
+              <div class="flex flex-row items-end gap-x-1 flex-shrink">
+                <p class={`font-bold text-white ${getFontSize(movie.title)}`}>{movie.title}</p>
+                <p class="font-light text-white text-sm">{movie.release_date.slice(0, 4)}</p>
+              </div>
+              <div class="flex flex-row justify-between items-end">
+                <div class="flex flex-col gap-y-0.5">
+                  <p class="text-xs font-light text-white">⭐{movie.vote_average}</p>
+                  <p class="ms-1 text-xs font-light text-white">{movie.vote_count} voted</p>
+                </div>
+                <button class="px-2 h-6 bg-yellow-500 font-medium text-black">+</button>
+              </div>
+            </div>
+          </div>
+        {/each}
+      {/if}
+    </div>        
+  </div>
 </main>
