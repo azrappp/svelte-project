39975282647b37b71f157d2ad6f93cd1dbffc7d3
azrappp <118091170+azrappp@users.noreply.github.com>
2024-09-04 21:24:16 +0700

    fixcard home card UI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,8 +10,12 @@
   let errorTopRated = null;
   let errorGenres = null;
   let activeMovieIndex = 0;
+  let interval;
 
   onMount(async () => {
+    interval = setInterval(() => {
+      activeMovieIndex = (activeMovieIndex + 1) % homeMovies.length;
+    }, 5000);
     try {
       const dataGenres = await fetchGenres(); // Ambil daftar genre
       genres = dataGenres.genres;
@@ -34,6 +38,7 @@
       }
       console.error(err);
     }
+    return () => clearInterval(interval);
   });
 
   const setActiveMovie = (index) => {
@@ -62,9 +67,12 @@
   .scrollbar-hidden::-webkit-scrollbar {
   display: none;
 }
+.transition-transform {
+    transition: transform 1s ease-in-out;
+  }
 
 </style>
-<nav class="font-sans flex flex-row border-b border-black border-opacity-10 bg-white backdrop-blur-lg bg-opacity-10 w-full px-8 py-4 items-center justify-between">
+<nav class="font-sans flex flex-row border-b border-black border-opacity-10 bg-white backdrop-blur-lg bg-opacity-10 w-full px-28  py-4 items-center justify-between">
   <div class="text-black font-bold">WatchMovie</div>
   <div class="flex flex-row gap-x-4">
     <a href="/movie" class="text-black font-normal font-san text-sm">Movie</a>
@@ -74,41 +82,41 @@
   </div>
 </nav>
 
-<main class="mx-8 md:mx-16 lg:mx-[80px] pt-6 items-center">
+<main class="mx-16 md:mx-32 lg:mx-36 pt-6 items-center">
   <!-- Hero Section -->
-  <div class="relative h-[545px] w-full flex items-center text-left bg-cover bg-center rounded-2xl"
-    style="background-image: url('https://image.tmdb.org/t/p/w1280{homeMovies[activeMovieIndex]?.backdrop_path}');">
-    <div class="font-sans absolute top-0 right-0 bottom-0 left-0 bg-gradient-to-r from-gray-900 to-gray-700/10 rounded-lg"></div>
-    <div class="flex flex-col justify-between w-full h-full py-16 mx-11">
-      <div class="flex justify-end">
-        {#if !errorPopular && homeMovies.length > 0}
-          <div class=" md:px-7 z-10 flex flex-col items-start gap-2 max-w-[402px]">
-            <p class="line-clamp-2 text-ellipsis overflow-hidden font-normal text-white text-xl">
-              {getGenreNames(popularMovies[activeMovieIndex].genre_ids)}
-            </p>
-            <p class="line-clamp-2 text-ellipsis overflow-hidden font-bold text-white text-4xl">
-              {homeMovies[activeMovieIndex].title}
-            </p>
-            <p class="text-xl text-white">⭐{homeMovies[activeMovieIndex].vote_average}</p>
-            <p class="font-grey text-gray-300 font-normal text-xl line-clamp-4">
-              {homeMovies[activeMovieIndex].overview}
-            </p>
-            <button class="px-3 py-2 text-black font-semibold bg-yellow-500 text-xl rounded-md">Add to Playlist +</button>
-          </div>
-        {:else if errorPopular}
-          <p class="text-red-500">Error: {errorPopular}</p>
-        {/if}
-      </div>
-      <div class="flex flex-row gap-x-1 justify-center">
-        {#each homeMovies as movie, index}
+  <div class="relative h-[460px]   w-full flex items-center text-left bg-cover bg-center rounded-2xl transition-transform duration-1000 ease-in-out"
+  style="background-image: url('https://image.tmdb.org/t/p/w1280{homeMovies[activeMovieIndex]?.backdrop_path}');">
+  <div class="font-sans absolute top-0 right-0 bottom-0 left-0 bg-gradient-to-r from-gray-900 to-gray-700/10 rounded-lg"></div>
+  <div class="flex flex-col justify-between w-full h-full py-6 mx-11">
+    <div class="flex  justify-end items-center mt-2">
+      {#if !errorPopular && homeMovies.length > 0}
+        <div class="md:px-7 z-10 flex flex-col items-start gap-2 max-w-[402px]">
+          <p class="line-clamp-2 text-ellipsis overflow-hidden font-normal text-white text-xl">
+            {getGenreNames(homeMovies[activeMovieIndex].genre_ids)}
+          </p>
+          <p class="line-clamp-2 text-ellipsis overflow-hidden font-bold text-white text-4xl">
+            {homeMovies[activeMovieIndex].title}
+          </p>
+          <p class="text-xl text-white">⭐{homeMovies[activeMovieIndex].vote_average}</p>
+          <p class="font-grey text-gray-300 font-normal text-xl line-clamp-4">
+            {homeMovies[activeMovieIndex].overview}
+          </p>
+          <button class="px-3 py-2 text-black font-semibold bg-yellow-500 text-xl rounded-md">Add to Playlist +</button>
+        </div>
+      {:else if errorPopular}
+        <p class="text-red-500">Error: {errorPopular}</p>
+      {/if}
+    </div>
+    <div class="flex flex-row gap-x-1 justify-center">
+      {#each homeMovies as movie, index}
         <button
           class="w-10 h-2 bg-white opacity-60 cursor-pointer {index === activeMovieIndex ? 'bg-opacity-100' : 'bg-opacity-20 '}"
           on:click={() => setActiveMovie(index)}
         ></button>
       {/each}
-      </div>
     </div>
   </div>
+</div>
 
 
     <!-- Movies Section -->
